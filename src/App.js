41710c6170b89ac4db72fb5236787639f412f930
Gaurab Ganguly <gaurabg.tih@gmail.com>
2024-02-13 01:34:30 +0530

    Initial folder structure commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./application/pages/Layout";
+import Home from "./application/pages/Home";
+import Blogs from "./application/pages/Blogs";
+import Contact from "./application/pages/Contact";
+import NoPage from "./application/pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
